47e9c2311352e6d8f395442a90dcabf3a9ce4625
EdwarH13 <edwar.13@outlook.co.il>
2024-01-27 20:47:08 +0200

    did some changes for classes
    in class running game i changed the type of playerplacement from int[] to int
    
    also in snakes class i removed the postion of snake because it was the same as the startofsnake
    
    also put a note in it please read it

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,53 +1,68 @@
 package model;
 
 public class Player {
-	  private String nickName;
-	    private Color colorPlayer;
-	    private int winCount;
+	private String nickName;
+	private String colorPlayer;
+	private int winCount;
 
-	    
-	    public String getNickName() {
-			return nickName;
+	public Player(String nickName, String colorPlayer, int winCount) {
+		super();
+		this.nickName = nickName;
+		this.colorPlayer = colorPlayer;
+		this.winCount = winCount;
+	}
+
+	public boolean checkNameExist(String name) {
+
+		if (this.nickName.equals(name)) {
+			System.out.println(NAME_SUCCESSFULLY_RECEIVED_MESSAGE);
+			return true;
 		}
+		System.out.println(NAME_FOUND_CHANGE_MESSAGE);
+		return false;
 
-		public void setNickName(String nickName) {
-			this.nickName = nickName;
+	}
+
+	// color type might be changed later once we apply GUI
+	public boolean checkColorExist(String color) {
+
+		if (this.colorPlayer.equals(color)) {
+			System.out.println(COLOR_SUCCESSFULLY_RECEIVED_MESSAGE);
+			return true;
 		}
+		System.out.println(COLOR_FOUND_CHANGE_MESSAGE);
+		return false;
 
-		public Color getColorPlayer() {
-			return colorPlayer;
-		}
+	}
 
-		public void setColorPlayer(Color colorPlayer) {
-			this.colorPlayer = colorPlayer;
-		}
+	public String getNickName() {
+		return nickName;
+	}
 
-		public int getWinCount() {
-			return winCount;
-		}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
 
-		public void setWinCount(int winCount) {
-			this.winCount = winCount;
-		}
+	public String getColorPlayer() {
+		return colorPlayer;
+	}
 
-		
-		public Player(String nickName, Color colorPlayer, int winCount) {
-			super();
-			this.nickName = nickName;
-			this.colorPlayer = colorPlayer;
-			this.winCount = winCount;
-		}
+	public void setColorPlayer(String colorPlayer) {
+		this.colorPlayer = colorPlayer;
+	}
 
-		public boolean checkNameExist(String name) {
-	         return false;}
+	public int getWinCount() {
+		return winCount;
+	}
 
-	    public boolean checkColorExist(String color) {
-	           return false; }
+	public void setWinCount(int winCount) {
+		this.winCount = winCount;
+	}
 
-	    // Notifications
-	    public static final String NAME_FOUND_CHANGE_MESSAGE = "The name was found, please change it.";
-	    public static final String NAME_SUCCESSFULLY_RECEIVED_MESSAGE = "The name was successfully received.";
-	    public static final String COLOR_FOUND_CHANGE_MESSAGE = "Please change the color found.";
-	    public static final String COLOR_SUCCESSFULLY_RECEIVED_MESSAGE = "The color was successfully received.";
+	// Notifications
+	public static final String NAME_FOUND_CHANGE_MESSAGE = "The name was found, please change it.";
+	public static final String NAME_SUCCESSFULLY_RECEIVED_MESSAGE = "The name was successfully received.";
+	public static final String COLOR_FOUND_CHANGE_MESSAGE = "Please change the color found.";
+	public static final String COLOR_SUCCESSFULLY_RECEIVED_MESSAGE = "The color was successfully received.";
 
 }
