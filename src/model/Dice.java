358f142dbbdf0577a322eb07691593bf04b426a5
hala abd alkader <7ala20072001@gmail.com>
2024-02-04 22:41:32 +0200

    Update Dice.java

--- src/model/Dice.java
+++ src/model/Dice.java
@@ -2,6 +2,8 @@ package model;
 
 import java.util.Random;
 
+import Utils.DiffLevel;
+
 public class Dice {
 	private static int auto = 0;
 	private int Id;
@@ -34,7 +36,33 @@ public class Dice {
 		this.number = random.nextInt(10) + 1;
 		return number;
 	}
-
+	/*
+	public int roll(DiffLevel level) {	
+		Random random = new Random();
+		if(level.equals(DiffLevel.hard)||level.equals(DiffLevel.medium))
+		{
+			this.number = random.nextInt(10) + 1;
+			if (number <= 7)
+				return number;
+			else {
+				if (number % 4 == 0)
+					return 10;
+				else if (number % 2 == 0)
+					return 9;
+				else
+					return 8;
+		}
+		}
+		if(level.equals(DiffLevel.easy))
+		{
+			this.number = random.nextInt(8) + 1;
+				return number;
+		}
+		return 0;			
+	}
+	*/
+	
+	
 	// Display an appropriate message
 	public void showTheResult() {
 		System.out.println(DICE_MESSAGES[number-1]);
