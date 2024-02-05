6517e063676626fa47e636dc67be2e401fe5a960
snhm47 <salahhanna86@gmail.com>
2024-02-05 09:16:45 +0200

    Main commite

--- src/View/MainScreen.java
+++ src/View/MainScreen.java
@@ -1,6 +1,5 @@
 package View;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -15,6 +14,10 @@ import java.awt.event.ActionEvent;
 
 public class MainScreen extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
