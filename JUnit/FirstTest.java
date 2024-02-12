70b180da43bade0044b3b793d923d16cb78057be
Bayan Yahia <142543434+bayanyahia@users.noreply.github.com>
2024-02-12 14:19:02 +0200

    Create FirstTest.java
    import first test

---
+++ JUnit/FirstTest.java
@@ -0,0 +1,17 @@
+
+import org.junit.Test;
+import model.Player;
+import static org.junit.Assert.*;
+
+
+public class FirstTest {
+
+	//Test 1- checks when creating a new user the high score is equal to 0.
+	@Test   
+	public void initialHighScoreTest() {  		
+		Player u=new Player("arwad", Utils.Color.RED, 0);
+		int expectedResult = 0 ;
+		int actualResult = u.getWinCount();
+		assertEquals(expectedResult, actualResult);
+	}
+}
