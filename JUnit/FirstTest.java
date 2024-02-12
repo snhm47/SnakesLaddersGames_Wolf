cbea9b7dc2ab11f305cd7fa024f521321ddd8941
hala abd alkader <7ala20072001@gmail.com>
2024-02-12 16:37:36 +0200

    add JSON Test

--- JUnit/FirstTest.java
+++ JUnit/FirstTest.java
@@ -14,4 +14,34 @@ public class FirstTest {
 		int actualResult = u.getWinCount();
 		assertEquals(expectedResult, actualResult);
 	}
+@Test	
+	// Test 2- checks if the import question from json file 
+	public void readQuestionsFromJSON() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("WolfQuestionsDB.json"));
+            String line;
+            StringBuilder jsonString = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                jsonString.append(line);
+            }
+
+            reader.close();
+
+            // המרת המחרוזת ל־JSONArray
+            JSONArray jsonArray = new JSONArray(jsonString.toString());
+            
+            // עבור על כל איבר ב־JSONArray ויצירת מופע של Question
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String questionText = jsonObject.getString("questionText");
+                String answer = jsonObject.getString("answer");
+                Question question = new Question(questionText, answer);
+                questions.add(question);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
 }
