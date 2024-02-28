d2992a01e54dae7f6e0052148ef0401bda85f3bb
Bayan Yahia <142543434+bayanyahia@users.noreply.github.com>
2024-02-28 12:36:01 +0200

    Create WrongPopupController.java

---
+++ src/Controller/WrongPopupController.java
@@ -0,0 +1,22 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class WrongPopupController {
+
+    @FXML
+    private Label messageLabel;
+
+    @FXML
+    private Button okButton;
+
+    public void setMessage(String message) {
+        messageLabel.setText(message);
+    }
+
+    @FXML
+    private void handleClose() {
+        // Close the popup window
+        okButton.getScene().getWindow().hide();
+    }
+}
