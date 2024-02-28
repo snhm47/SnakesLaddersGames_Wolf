20be2a8977d0abef11c4187d1fe7620dae3205c0
EdwarH13 <edwar.13@outlook.co.il>
2024-02-28 12:47:53 +0200

    fixed questions

--- src/model/Question.java
+++ src/model/Question.java
@@ -1,177 +1,98 @@
 package model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import Utils.DiffLevel;
 
 public class Question {
 
-	private static int count =0; // to defines the number of the question and runs automatically
+	private static int count = 0;
 	private int number;
-	private String text;
-	private ArrayList<String> answers=new ArrayList<String>();
-	private DiffLevel difficulty;
-	private String correctAnswer;
-	
-	public Question(String question,List<String> answers,String correct_ans,DiffLevel difficulty) {
+	private final StringProperty text;
+	private ArrayList<String> answers = new ArrayList<>();
+	private final StringProperty correctAnswer;
+	private final StringProperty difficulty;
+    private String textQ;
+
+	public Question(String question, List<String> answers, String correct_ans, String difficulty) {
 		count++;
-		this.number=count;
-		text=question;
-		for(String s:answers) {
+		this.number = count;
+		this.text = new SimpleStringProperty(question);
+		for (String s : answers) {
 			this.answers.add(s);
 		}
-		this.correctAnswer=correct_ans;
-		this.difficulty = DiffLevel.easy ;
-		if(difficulty.equals("2")) {
-			this.difficulty = DiffLevel.medium ;
-		}
-		if(difficulty.equals("3")) {
-			this.difficulty = DiffLevel.hard ;
-		}
+		this.correctAnswer = new SimpleStringProperty(correct_ans);
+		this.difficulty = new SimpleStringProperty(difficulty);
+	}
+
+	public Question(String questionText, String difficulty) {
+		this.text = new SimpleStringProperty(questionText);
+		this.difficulty = new SimpleStringProperty(difficulty);
+		this.answers = new ArrayList<>();
+		this.correctAnswer = new SimpleStringProperty("");
 	}
 
 	public int getNumber() {
 		return number;
 	}
 
-	public void setNumber(int number) {
-		this.number = number;
-	}
-
 	public String getText() {
+		return text.get();
+	}
+	
+	public String getTextQ() {
+		return text.get();
+	}
+	
+	public void setTextQ(String text) {
+		this.textQ= text;
+	}
+
+
+
+	public StringProperty questionProperty() {
 		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
-	}
-
-	public DiffLevel getDifficulty() {
-		return difficulty;
-	}
-
-	public void setDifficulty(DiffLevel difficulty) {
-		this.difficulty = difficulty;
 	}
 
 	public ArrayList<String> getAnswers() {
 		return answers;
 	}
-	
-	public void setAnswers(ArrayList<String> answers) {
-		if(!answers.equals(null)) {
-		for(String s:answers) {
-			if(!s.equals(null))
-			this.answers.add(s);
-		}
-		}
-	}
-
-
 
 	public String getCorrectAnswer() {
+		return correctAnswer.get();
+	}
+
+	public StringProperty correctAnswerProperty() {
 		return correctAnswer;
 	}
 
+	public String getDifficulty() {
+		return difficulty.get();
+	}
+
+	public StringProperty difficultyProperty() {
+		return difficulty;
+	}
+
+	public void setAnswers(ArrayList<String> answers) {
+		if (!answers.isEmpty()) {
+			this.answers = answers;
+		}
+	}
+
 	public void setCorrectAnswer(String correctAnswer) {
-		this.correctAnswer = correctAnswer;
+		this.correctAnswer.set(correctAnswer);
 	}
 
-	public Question(String text,  ArrayList<String> answers, String correctAnswer, DiffLevel difficulty) {
-		super();
-		count++;
-		this.number=count;
-		this.text = text;
-		this.difficulty = difficulty;
-		if(answers.equals(null)==false)
-		setAnswers(answers);
-		this.correctAnswer = correctAnswer;
+	public void setDifficulty(String difficulty) {
+		this.difficulty.set(difficulty);
 	}
 
-	// Methods
-	// Method to add the 4 answers option for a question
-//	public void addAnswers(String[] possibleAnswers) {
-//		if (possibleAnswers.length == 4) 
-//			{answers = new ArrayList<>();
-//			for (String answer : possibleAnswers) 
-//				answers.add(answer);}
-//		else 
-//			System.out.println("Error: You must add 4 possible answers to each question.");
-//		
-//	}
-
-	// Method to check if the question is in the data warehouse
-	public boolean checkQuestion(ArrayList<Question> questionDatabase) {
-		for (Question question : questionDatabase) {
-			if (this.text.equalsIgnoreCase(question.getText())) {
-				System.out.println("Qustion found!");
-				return true;
-			}
-		}
-		return false;
+	public void editQuestion(String newText, List<String> newAnswers, String newCorrectAnswer, String newDifficulty) {
+		this.text.set(newText);
+		this.setAnswers(new ArrayList<>(newAnswers));
+		this.setCorrectAnswer(newCorrectAnswer);
+		this.setDifficulty(newDifficulty);
 	}
-	
-	// Method to add a question to the database
-    // Check if the number of possible answers is exactly 4
-    // Check if the question already exists in the database
-
-	public void addQuestionAndAnswers(ArrayList<Question> questionDatabase, String[] possibleAnswers) {
-	    if (possibleAnswers.length == 4) {
-	        if (!checkQuestion(questionDatabase)) {
-	            this.answers = new ArrayList<>(Arrays.asList(possibleAnswers));
-	            questionDatabase.add(this);
-	            System.out.println("Question successfuly added");
-	        } else {
-	            System.out.println("Question already exists in the database.");
-	        }
-	    } else {
-	        System.out.println("Error: You must provide exactly 4 possible answers.");
-	    }
-	}
-	
-	// Method to display all questions in the database
-	public void displayAllQuestions(ArrayList<Question> questionDatabase) {
-		for (Question question : questionDatabase) {
-			System.out.println("Number: " + question.getNumber());
-			System.out.println("Text: " + question.getText());
-			System.out.println("Difficulty: " + question.getDifficulty());
-			System.out.println("Answers: " + question.getAnswers());
-			System.out.println("Correct Answer: " + question.getCorrectAnswer());
-		}
-	}
-
-	// Method to remove a question and there answers from the database
-    // Check if the current question is the one to be removed
-    // Remove the question and its associated answers
-	// Stop iterating once the associated question is found and processed
-	public void removeQuestionAndAnswers(ArrayList<Question> questionDatabase) {
-	    for (Question question : questionDatabase) {
-	        if (question.equals(this)) {
-	            questionDatabase.remove(this);
-	            this.answers = null;
-	            System.out.println("Question successfuly removed");
-	            return;
-	        }
-	    }
-	    System.out.println("Question not found in the database.");
-	}
-
-	// Method to edit an existing question in the database
-	public void editQuestion(ArrayList<Question> questionDatabase, String newText, String[] newAnswers,
-			String newCorrectAnswer) {
-		if (checkQuestion(questionDatabase)) {
-			this.setText(newText);
-			ArrayList<String>answers = new ArrayList<String>();
-			for(String an : newAnswers) {
-				answers.add(an);
-			}
-			this.setAnswers(answers);
-			this.setCorrectAnswer(newCorrectAnswer);
-			System.out.println("Question updated successfully");
-		}
-
-	}
-
 }
